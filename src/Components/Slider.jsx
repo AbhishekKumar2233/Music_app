3fde52c5078ea8f054141e1f60615068236c5652
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-02-09 16:15:58 +0530

    Slider done

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,18 +1,50 @@
 import React from "react";
 import Slider from "react-slick";
 
+function SampleNextArrow(props) {
+  const { className, onClick } = props;
+  return <div className={className} onClick={onClick} />;
+}
+
+function SamplePrevArrow(props) {
+  const { className, onClick } = props;
+  return (
+    <div
+      className={className}
+      // style={{ ...style, display: "block", background: "green" }}
+      onClick={onClick}
+    />
+  );
+}
+
 export default function SliderItem() {
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
+    lazyLoad: true,
+    slidesToScroll: 1,
+    autoplaySpeed: 100,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />
   };
   return (
     <div>
       <h2> Single Item</h2>
       <Slider {...settings} className="slider">
+        <div>
+          <img
+            src="https://img.wynk.in/unsafe/720x251/filters:no_upscale():format(webp)/https://s3.ap-south-1.amazonaws.com/discovery-prod-arsenal/arsenal/artworks/620357bbe4ab4748300ef040/BANNER_1293525908732763.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            src="https://img.wynk.in/unsafe/720x251/filters:no_upscale():format(webp)/https://s3.ap-south-1.amazonaws.com/discovery-prod-arsenal/arsenal/artworks/620357bbe4ab4748300ef040/BANNER_1293525908732763.jpg"
+            alt=""
+          />
+        </div>
         <div>
           <img
             src="https://img.wynk.in/unsafe/720x251/filters:no_upscale():format(webp)/https://s3.ap-south-1.amazonaws.com/discovery-prod-arsenal/arsenal/artworks/620357bbe4ab4748300ef040/BANNER_1293525908732763.jpg"
