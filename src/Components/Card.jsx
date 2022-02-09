a6ee7beedf25f30b275c67b257da3d229cc49fb1
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-02-10 00:37:14 +0530

    Lazy loading 1 sec

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+
 import PlayCircleFilledWhiteIcon from "@mui/icons-material/PlayCircleFilledWhite";
 
 export default function Card(props) {
   return (
-    <div className="card">
-      <img alt={props.name} src={props.img} />
-      <button id="button">
-        <PlayCircleFilledWhiteIcon />
-      </button>
-      <h2>{props.name}</h2>
-    </div>
+    <>
+      <div className="card">
+        <img alt={props.name} src={props.img} />
+        <h2>{props.name}</h2>
+      </div>
+      {/* <div className="card-hidden">
+        <button id="button">
+          <PlayCircleFilledWhiteIcon />
+        </button>
+      </div> */}
+    </>
   );
 }
--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import Card from "./Card";
 import Data from "./Data";
 import "../styles.css";
 
+/* wait 1s to render component */
+const CustomComponent = React.lazy(
+  () =>
+    new Promise((resolve, reject) =>
+      setTimeout(() => resolve(import("./Card")), 1000)
+    )
+);
+
 export default function Content() {
   return (
     <div className="content">
-      {Data.map((card) => (
+      <Suspense fallback={<div>Loading</div>}>
+        {Data.map((card) => (
+          <CustomComponent name={card.name} img={card.img} />
+        ))}
+      </Suspense>
+      {/* {Data.map((card) => (
         <Card name={card.name} img={card.img} />
-      ))}
+      ))} */}
     </div>
   );
 }
